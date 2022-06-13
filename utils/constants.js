d3664470a935bc88d53aabebf34d5072fc92de96
Paul513607 <paul.samson42@gmail.com>
2022-06-13 16:16:31 +0300

    quick fix

--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,6 @@ module.exports = {
     port: 3000,
     hostUrl: 'http://localhost:3000',
     db_url: 'mongodb+srv://admin:admin@mawt.0d0jq.mongodb.net/?retryWrites=true&w=majority',
-    secret: process.env.DB_SECRET,
+    secret: 'ana_are_mere',
     rounds: 5
 }
