33a204c267e93e3fcb1a0946252f0d41eb92f99b
Paul513607 <paul.samson42@gmail.com>
2022-06-14 01:34:15 +0300

    added CRUD operations for hidroplants

--- routes/hidroplant.js
+++ routes/hidroplant.js
@@ -7,5 +7,6 @@ router.get('', hidroplantController.getAll)
 router.post('', hidroplantController.createHidroplant)
 router.put('/:id', hidroplantController.updateHidroplant)
 router.delete('/:id', hidroplantController.deleteHidroplant)
+router.get('/name', hidroplantController.getByName)
 
 module.exports = router
