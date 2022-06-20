36629a1623875024b2ab2d3229397a2f86ce65ae
mandreea21 <mmariandreea01@gmail.com>
2022-06-21 02:23:11 +0300

    MapPage mai trebuie imbunatatit
    lucrez maine la ea :+1:

---
+++ frontend/public/scripts/initialiseSearchBar.js
@@ -0,0 +1,71 @@
+import { BASE_URL } from "./constants.js";
+
+const authToken = sessionStorage.getItem("JWT_TOKEN");
+
+
+let lat1;
+let lon1;
+
+const getHidropowerPlantList = async() => {
+    try {
+        const res = await axios.get(`${BASE_URL}/hidroplants`, {
+            headers: {
+                Authorization: `Bearer ${authToken}`
+            }
+        })
+
+        return res.data.hidroplants;
+    } catch (err) {
+        alert('Something went wrong while fetching the data: ' + err.response.data.message);
+        return null;
+    }
+}
+
+const hidropowerList = async() => {
+    const hidroplants = await getHidropowerPlantList();
+    if (hidroplants == null) {
+        return;
+    }
+
+    const hidroList = document.querySelector('#dropdownHidro');
+
+    for (let counter = 0; counter < hidroplants.length; counter++) {
+       
+        
+        let item=document.createElement("li");
+        var inputA=document.createElement("a");
+        let name=document.createTextNode(`${hidroplants[counter].name}`);
+       
+    
+        inputA.appendChild(name);
+        item.appendChild(inputA);
+        hidroList.appendChild(item);
+
+        if(localStorage.getItem("hidro")===`${hidroplants[counter].name}`){
+        
+        lat1=`${hidroplants[counter].lat_res}`;
+        lon1=`${hidroplants[counter].long_res}`;
+        
+        localStorage.setItem("lat1",`${hidroplants[counter].lat_res}`);
+        localStorage.setItem("lon1",`${hidroplants[counter].long_res}`);
+   
+    }
+    }
+}
+
+hidropowerList();
+
+const getLat1 =() =>{
+    
+    return `${lat1}`;
+}
+const getLon1 =() =>{
+    return `${lon1}`;
+}
+
+ export {getLat1, getLon1};
+
+
+
+
+
--- frontend/public/scripts/map.page.js
+++ frontend/public/scripts/map.page.js
@@ -1,13 +1,28 @@
-let mapOptions = {
-    center:[51.958, 9.141],
-    zoom:10
+import { getLat1, getLon1} from './initialiseSearchBar.js';
+
+const createMap = () => {
+
+    let mapOptions = {
+        center:[0,0],
+        zoom:1
+    }
+
+
+    let map = new L.map('map' , mapOptions);
+
+    let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    map.addLayer(layer);
+    let lat1=getLat1();
+    let lon1=getLon1();
+
+
+
+    let marker = new L.Marker([ localStorage.getItem("lat1"), localStorage.getItem("lon1") ]);
+
+    let info=`Name: ${localStorage.getItem("hidro")}; Lat: ${localStorage.getItem("lat1")}; Long: ${localStorage.getItem("lon1")};`;
+
+    marker.bindPopup(info);
+
+    marker.addTo(map);
 }
-
-
-let map = new L.map('map' , mapOptions);
-
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-map.addLayer(layer);
-
-let marker = new L.Marker([51.958, 9.141]);
-marker.addTo(map);
+createMap();
