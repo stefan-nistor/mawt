2d78d9bcbea87d69a3a4ce0653d8f73592c132d8
mandreea21 <mmariandreea01@gmail.com>
2022-06-24 00:49:50 +0300

    admin home

--- frontend/public/scripts/home.js
+++ frontend/public/scripts/home.js
@@ -4,35 +4,42 @@ const authToken = sessionStorage.getItem("JWT_TOKEN");
 
 // route protection
 if (authToken == null) {
-    location.href = "./login.html";
+  location.href = "./login.html";
 }
 
-const getHidropowerPlantList = async() => {
-    try {
-        const res = await axios.get(`${BASE_URL}/hidroplants`, {
-            headers: {
-                Authorization: `Bearer ${authToken}`
-            }
-        })
+const getHidropowerPlantList = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/hidroplants`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
 
-        return res.data.hidroplants;
-    } catch (err) {
-        alert('Something went wrong while fetching the data: ' + err.response.data.message);
-        return null;
-    }
-}
+    return res.data.hidroplants;
+  } catch (err) {
+    alert(
+      "Something went wrong while fetching the data: " +
+        err.response.data.message
+    );
+    return null;
+  }
+};
 
-const addHidropowerList = async() => {
-    const hidroplants = await getHidropowerPlantList();
-    if (hidroplants == null) {
-        return;
-    }
+const addHidropowerList = async () => {
+  const hidroplants = await getHidropowerPlantList();
+  if (hidroplants == null) {
+    return;
+  }
 
-    const hidroList = document.querySelector('.hidro-list');
+  const hidroList = document.querySelector(".hidro-list");
 
-    for (let counter = 0; counter < hidroplants.length; counter++) {
-        const description = hidroplants[counter].purpose ? hidroplants[counter].purpose : "Hidroplant";
-        let item = `<a href="./hidroPower-Page.html?name=${hidroplants[counter].name}">
+  for (let counter = 0; counter < hidroplants.length; counter++) {
+    const description = hidroplants[counter].purpose
+      ? hidroplants[counter].purpose
+      : "Hidroplant";
+    let item = `<a href="./hidroPower-Page.html?name=${
+      hidroplants[counter].name
+    }">
                         <div class="hidro-one">
                             <div class="white-box">
                                 ${counter + 1}
@@ -45,10 +52,14 @@ const addHidropowerList = async() => {
                                     ${description}
                                 </div>
                             </div>
+                            <button id="trash"><span class="trash">
+                            <span></span>
+                             <i></i>
+                             </span></button>
                         </div>
                     </a>`;
-        hidroList.innerHTML += item;
-    }
-}
+    hidroList.innerHTML += item;
+  }
+};
 
 addHidropowerList();
