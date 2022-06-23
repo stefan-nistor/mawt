00048c9af9c24daa40f7728769359993d6a50f9e
mandreea21 <mmariandreea01@gmail.com>
2022-06-23 17:02:49 +0300

    statistics cu functionalitate

--- frontend/public/scripts/statistics.js
+++ frontend/public/scripts/statistics.js
@@ -1,50 +1,92 @@
-let canvasElement = document.getElementById("barChart");
+import { BASE_URL } from "./constants.js";
 
-let config = {
-    type:"bar",
-    data:{ 
-        labels: ["first","second","third","forth"],
-        datasets: [{ 
-            label: "Electric capacity", 
-            data: [ 5 , 2 , 12 , 19 , 3 , 23 , 4 , 24 , 6 , 10],
-            backgroundColor:["#c5ce62","#c5ce62","#c5ce62","#c5ce62"],
-            borderColor: ["#f5edf0"],
-            borderWidth: 1,
-        }]
-    },
+const authToken = sessionStorage.getItem("JWT_TOKEN");
 
-};
-
-let barChart = new Chart(canvasElement, config);
-
-let canvasElement2 = document.getElementById("pieChart");
-
-let config2 = {
-    type:"pie",
-    data:{ 
-        labels: ["first","second","third","forth"],
-        datasets: [{ 
-            label: "Electric capacity", 
-            data: [ 5 , 2 , 12 , 19 , 3 , 23 , 4 , 24 , 6 , 10],
-            backgroundColor:["#c5ce62","#c5ce62","#c5ce62","#c5ce62"],
-            borderColor: ["#f5edf0"],
-            borderWidth: 1,
-        }]
-    },
-
-};
-
-let pieChart = new Chart(canvasElement2, config2);
-
-const changeView = () => {
- 
-    if(document.getElementById("barChart").style.display==="block")
-    {
-        document.getElementById("barChart").style.display="none";
-        document.getElementById("pieChart").style.display="block";
-    }
-    else{
-        document.getElementById("barChart").style.display="block";
-        document.getElementById("pieChart").style.display="none";
-    }
+// route protection
+if (authToken == null) {
+  location.href = "./login.html";
 }
+
+let pieChart;
+let barChart;
+
+const createBarChart = (data) => {
+  let canvasElement = document.getElementById("barChart");
+
+  let namesHidro = data.map((hp) => hp.name);
+  let elecCapVal = data.map((hp) => hp.elec_cap);
+
+  let config = {
+    type: "bar",
+    data: {
+      labels: namesHidro,
+      datasets: [
+        {
+          label: "Electric capacity",
+          data: elecCapVal,
+          backgroundColor: ["#c5ce62", "#c5ce62", "#c5ce62", "#c5ce62"],
+          borderColor: ["#f5edf0"],
+          borderWidth: 1,
+        },
+      ],
+    },
+  };
+  barChart = new Chart(canvasElement, config);
+};
+
+const createPieChart = (data) => {
+  let canvasElement2 = document.getElementById("pieChart");
+
+  let namesHidro = data.map((hp) => hp.name);
+  let elecCapVal = data.map((hp) => hp.elec_cap);
+
+  let config2 = {
+    type: "pie",
+    data: {
+      labels: namesHidro,
+      datasets: [
+        {
+          label: "Electric capacity",
+          data: elecCapVal,
+          backgroundColor: ["#c5ce62", "#c5ce62", "#c5ce62", "#c5ce62"],
+          borderColor: ["#f5edf0"],
+          borderWidth: 1,
+        },
+      ],
+    },
+  };
+  pieChart = new Chart(canvasElement2, config2);
+};
+
+const getGraphs = async () => {
+  try {
+    const howMany = 10;
+    const res = await axios.get(
+      `${BASE_URL}/hidroplants/top?howMany=${howMany}`,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
+
+    const data = res.data.hidroplants;
+    console.log(data);
+
+    createBarChart(data);
+    createPieChart(data);
+  } catch (err) {
+    alert(" Error: " + err.response.data.message);
+  }
+};
+getGraphs();
+
+const changeBtn = document.addEventListener("click", () => {
+  if (document.getElementById("barChart").style.display === "block") {
+    document.getElementById("barChart").style.display = "none";
+    document.getElementById("pieChart").style.display = "block";
+  } else {
+    document.getElementById("barChart").style.display = "block";
+    document.getElementById("pieChart").style.display = "none";
+  }
+});
