0abe05ca6bda57a6bebebea5e1bc96a659b7ab46
mandreea21 <mmariandreea01@gmail.com>
2022-06-24 17:39:44 +0300

    trash

--- frontend/public/scripts/homeAdmin-Page.js
+++ frontend/public/scripts/homeAdmin-Page.js
@@ -4,39 +4,45 @@ const authToken = sessionStorage.getItem("JWT_TOKEN");
 
 // route protection
 if (authToken == null) {
-    location.href = "./login.html";
+  location.href = "./login.html";
 }
 
-const getHidropowerPlantList = async() => {
-    try {
-        const res = await axios.get(`${BASE_URL}/hidroplants`, {
-            headers: {
-                Authorization: `Bearer ${authToken}`,
-            },
-        });
+const getHidropowerPlantList = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/hidroplants`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
 
-        return res.data.hidroplants;
-    } catch (err) {
-        alert(
-            "Something went wrong while fetching the data: " +
-            err.response.data.message
-        );
-        return null;
-    }
+    return res.data.hidroplants;
+  } catch (err) {
+    alert(
+      "Something went wrong while fetching the data: " +
+        err.response.data.message
+    );
+    return null;
+  }
 };
 
-const addHidropowerList = async() => {
-    const hidroplants = await getHidropowerPlantList();
-    if (hidroplants == null) {
-        return;
-    }
+const addHidropowerList = async () => {
+  const hidroplants = await getHidropowerPlantList();
+  if (hidroplants == null) {
+    return;
+  }
 
-    const hidroList = document.querySelector(".hidro-list");
+  const hidroList = document.querySelector(".hidro-list");
 
-    for (let counter = 0; counter < hidroplants.length; counter++) {
-        const description = hidroplants[counter].purpose ? hidroplants[counter].purpose : "Hidroplant";
-        let item = `<div id="${hidroplants[counter].name}">
-                      <a href="./hidroPower-Page.html?name=${hidroplants[counter].name}">
+  for (let counter = 0; counter < hidroplants.length; counter++) {
+    const description = hidroplants[counter].purpose
+      ? hidroplants[counter].purpose
+      : "Hidroplant";
+    let item = `<div id="${
+      hidroplants[counter].name
+    }" style="text-decoration:none">
+                      <a href="./hidroPower-Page.html?name=${
+                        hidroplants[counter].name
+                      }">
                           <div class="hidro-one">
                               <div class="white-box">
                                   ${counter + 1}
@@ -56,50 +62,53 @@ const addHidropowerList = async() => {
                         <span></span>
                       </button>
                     </div>`;
-        hidroList.innerHTML += item;
-    }
+    hidroList.innerHTML += item;
+  }
 };
 
-const getHidroplantForName = async(name) => {
-    try {
-        const res = await axios.get(`${BASE_URL}/hidroplants/name?name=${name}`, {
-            headers: {
-                Authorization: `Bearer ${authToken}`
-            },
-        })
+const getHidroplantForName = async (name) => {
+  try {
+    const res = await axios.get(`${BASE_URL}/hidroplants/name?name=${name}`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
 
-        return res.data.hidroplant;
-    } catch (err) {
-        alert("Error: " + err.response.data.message);
-    }
-}
+    return res.data.hidroplant;
+  } catch (err) {
+    alert("Error: " + err.response.data.message);
+  }
+};
 
 const addTrashButtonFunctionalities = () => {
-    const trashButtons = document.getElementsByClassName("trash");
-    for (const trashBtn of trashButtons) {
-        trashBtn.addEventListener('click', async() => {
-            try {
-                const name = trashBtn.parentElement.id;
-                const hidroplant = await getHidroplantForName(name);
-                const res = await axios.delete(`${BASE_URL}/hidroplants/${hidroplant._id}`, {
-                    headers: {
-                        Authorization: `Bearer ${authToken}`
-                    },
-                    data: {}
-                })
+  const trashButtons = document.getElementsByClassName("trash");
+  for (const trashBtn of trashButtons) {
+    trashBtn.addEventListener("click", async () => {
+      try {
+        const name = trashBtn.parentElement.id;
+        const hidroplant = await getHidroplantForName(name);
+        const res = await axios.delete(
+          `${BASE_URL}/hidroplants/${hidroplant._id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+            data: {},
+          }
+        );
 
-                alert("Success: " + res.data.message);
-                location.reload();
-            } catch (err) {
-                alert("Error: " + err.response.data.message);
-            }
-        });
-    }
+        alert("Success: " + res.data.message);
+        location.reload();
+      } catch (err) {
+        alert("Error: " + err.response.data.message);
+      }
+    });
+  }
 };
 
-const addData = async() => {
-    await addHidropowerList();
-    addTrashButtonFunctionalities();
-}
+const addData = async () => {
+  await addHidropowerList();
+  addTrashButtonFunctionalities();
+};
 
 addData();
